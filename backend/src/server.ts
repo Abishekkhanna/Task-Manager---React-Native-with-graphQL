35fe9db0d24e2f9c4358a877e216a1e6c29e66f1
Abishek Khanna M S <75411526+Abishekkhanna@users.noreply.github.com>
2026-09-15 21:51:05 +0530

    feat: configure prisma database layer

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,6 +3,8 @@ import { startStandaloneServer } from "@apollo/server/standalone";
 
 import typeDef from "./api/graphql/typeDef";
 import resolver from "./api/graphql/resolver";
+import getContext from "./api/graphql/context";
+import prisma from "./utils/database/prisma";
 
 const server = new ApolloServer({
   typeDefs: typeDef,
